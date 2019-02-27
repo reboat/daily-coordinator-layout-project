30b140ffebd47bb30ce03353e58d0d781f648622
李欣科 <lixink@8531.cn>
2019-02-27 10:32:20 +0800

    修改滑动效果

--- library/src/main/java/cn/daily/android/behavior/DailyCoordinatorBehavior.java
+++ library/src/main/java/cn/daily/android/behavior/DailyCoordinatorBehavior.java
@@ -13,15 +13,18 @@ import cn.daily.android.widget.R;
 
 public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
     private static final String TAG = "DailyBehavior";
-    private View mHeaderView;
     private View mScrolledView;
     private View mScrolledAnchorView;
+    private View mFixedView;
     private int mScrolledWidth = 0;
     private int mVerticalDistance = 0;
     private int mRightSpace = 0;
     private int mMaxVerticalDistance = 0;
-    private int mFixedWidth =0;
-    private float mMinWidth=0;
+    private int mFixedWidth = 0;
+    private float mMinWidth = 0;
+    private int mConsumedY = 0;
+    private int mFixedTop = 0;
+    private float scale;
 
     public DailyCoordinatorBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -37,20 +40,36 @@ public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
             child.getLocationOnScreen(location3);
             mVerticalDistance = location3[1] - location[1];
         }
+
+        if (mMaxVerticalDistance == 0) {
+            int[] location = new int[2];
+            mScrolledView.getLocationOnScreen(location);
+            int[] location2 = new int[2];
+            child.findViewById(R.id.fixed_root).getLocationOnScreen(location2);
+            mMaxVerticalDistance = location2[1] - location[1];
+        }
+
+        if (mFixedTop == 0) {
+            Rect rect = new Rect();
+            mFixedView.getGlobalVisibleRect(rect);
+            mFixedTop = rect.top;
+        }
         return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes, type);
     }
 
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed, int type) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
+
         Rect rect = new Rect();
-        mHeaderView.getLocalVisibleRect(rect);
-        float height = mMaxVerticalDistance - rect.top;
-        float scale = height / mMaxVerticalDistance;
-        ViewGroup.LayoutParams params1= mScrolledAnchorView.getLayoutParams();
-        params1.width= (int) (mMinWidth-mMinWidth*scale);
-        if(params1.width<0){
-            params1.width= (int) mMinWidth;
+        mFixedView.getGlobalVisibleRect(rect);
+        mConsumedY = mFixedTop - rect.top;
+        float height = mMaxVerticalDistance - mConsumedY;
+        scale = height / mMaxVerticalDistance;
+        ViewGroup.LayoutParams params1 = mScrolledAnchorView.getLayoutParams();
+        params1.width = (int) (mMinWidth - mMinWidth * scale);
+        if (mMinWidth * scale <= 0) {
+            params1.width = (int) mMinWidth;
         }
         mScrolledAnchorView.setLayoutParams(params1);
 
@@ -60,38 +79,41 @@ public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
             params.width = (int) mMinWidth;
         }
         mScrolledView.setLayoutParams(params);
-        mScrolledView.setTranslationY(mVerticalDistance - mVerticalDistance * scale);
-        mScrolledView.setTranslationX(mRightSpace - mRightSpace * scale);
-        if (rect.top < 0) {
+        mScrolledView.setTranslationX(params1.width);
+
+        int[] location = new int[2];
+        mScrolledAnchorView.getLocationOnScreen(location);
+        int[] location2 = new int[2];
+        mScrolledView.getLocationOnScreen(location2);
+
+        if (location[1] <= location2[1]) {
+            mScrolledView.setVisibility(View.INVISIBLE);
             mScrolledAnchorView.setVisibility(View.VISIBLE);
-            mScrolledView.setVisibility(View.GONE);
         } else {
-            mScrolledAnchorView.setVisibility(View.INVISIBLE);
             mScrolledView.setVisibility(View.VISIBLE);
+            mScrolledAnchorView.setVisibility(View.INVISIBLE);
         }
+
     }
 
     @Override
     public boolean onLayoutChild(CoordinatorLayout parent, AppBarLayout abl, int layoutDirection) {
         Log.e(TAG, "into--[onLayoutChild]");
-        mHeaderView = ((View) parent.getParent()).findViewById(R.id.header_view);
-        if (mMaxVerticalDistance == 0) {
-            mMaxVerticalDistance = mHeaderView.getMeasuredHeight();
-        }
         mScrolledView = ((View) parent.getParent()).findViewById(R.id.scrolled_view);
         if (mScrolledWidth == 0) {
             mScrolledWidth = mScrolledView.getMeasuredWidth();
-            ((View)mScrolledView.getParent()).bringToFront();
+            ((View) mScrolledView.getParent()).bringToFront();
         }
         mScrolledAnchorView = abl.findViewById(R.id.scrolled_anchor_view);
         if (mRightSpace == 0) {
             mRightSpace = ((View) parent.getParent()).findViewById(R.id.scrolled_right_view).getMeasuredWidth();
         }
-        if(mFixedWidth ==0){
-            mFixedWidth =abl.findViewById(R.id.fixed_root).getMeasuredWidth();
+        mFixedView = abl.findViewById(R.id.fixed_root);
+        if (mFixedWidth == 0) {
+            mFixedWidth = mFixedView.getMeasuredWidth();
         }
 
-        mMinWidth=parent.getContext().getResources().getDimension(R.dimen.scroll_view_min_width);
+        mMinWidth = parent.getContext().getResources().getDimension(R.dimen.scroll_view_min_width);
 
         return super.onLayoutChild(parent, abl, layoutDirection);
     }
