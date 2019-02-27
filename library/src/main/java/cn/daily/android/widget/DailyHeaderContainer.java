91babed2c3331a17dc9d198f2e1be064f79dd63c
李欣科 <lixink@8531.cn>
2019-02-27 17:10:06 +0800

    点击跳转

--- library/src/main/java/cn/daily/android/widget/DailyHeaderContainer.java
+++ library/src/main/java/cn/daily/android/widget/DailyHeaderContainer.java
@@ -3,7 +3,10 @@ package cn.daily.android.widget;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import cn.daily.news.biz.core.nav.Nav;
 
 public class DailyHeaderContainer extends FrameLayout {
 
@@ -24,5 +27,23 @@ public class DailyHeaderContainer extends FrameLayout {
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
         inflate(context,R.layout.search_layout,this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        findViewById(R.id.scrolled_view).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Nav.with(v.getContext()).toPath("/search/index");
+            }
+        });
+
+        findViewById(R.id.scrolled_right_view).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Nav.with(v.getContext()).toPath("/service/index");
+            }
+        });
     }
 }
