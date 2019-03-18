ca0eabe08a2ec4c5789928925c5445a6a6199e54
李欣科 <lixink@8531.cn>
2019-03-18 16:01:09 +0800

    计算padding

--- library/src/main/java/cn/daily/android/behavior/DailyCoordinatorBehavior.java
+++ library/src/main/java/cn/daily/android/behavior/DailyCoordinatorBehavior.java
@@ -6,7 +6,6 @@ import android.os.Build;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -64,6 +63,11 @@ public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
      */
     private int[] mAnchorLocation;
 
+    /**
+     * 背景图标的高度
+     */
+    private float mHeaderHeight = 0;
+
     private int mStatusBarHeight = 0;
 
     public DailyCoordinatorBehavior(Context context, AttributeSet attrs) {
@@ -111,28 +115,16 @@ public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
         return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes, type);
     }
 
-    boolean up = true;
-    int top = mFixedTop;
 
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed, int type) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type);
 //        Log.e(TAG, "into--[onNestedPreScroll]");
 
-
-        Rect headRect = new Rect();
-        mHeaderView.getGlobalVisibleRect(headRect);
         Rect rect = new Rect();
         mFixedView.getGlobalVisibleRect(rect);
 
-        if (rect.top <= top) {
-            up = true;
-        } else {
-            up = false;
-        }
-
         mConsumedY = mFixedTop - rect.top;
-        top = rect.top;
         float height = mMaxVerticalDistance - mConsumedY;
         mScale = height / mMaxVerticalDistance;
         ViewGroup.LayoutParams scrolledAnchorViewLayoutParams = mScrolledAnchorView.getLayoutParams();
@@ -163,47 +155,14 @@ public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
         }
 
 
+        //透明status bar add padding
+        float paddingScale = mConsumedY / mHeaderHeight;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            addPadding(headRect, rect, scrolledAnchorViewLayoutParams);
+            ViewGroup.LayoutParams paddingViewLayoutParams = mPaddingView.getLayoutParams();
+            paddingViewLayoutParams.height = (int) (paddingScale * mStatusBarHeight);
         }
-
-
     }
 
-    /**
-     * 带有透明status bar 防止遮挡tab文字
-     *
-     * @param headRect
-     * @param rect
-     * @param scrolledAnchorViewLayoutParams
-     */
-    private void addPadding(Rect headRect, Rect rect, ViewGroup.LayoutParams scrolledAnchorViewLayoutParams) {
-        if (scrolledAnchorViewLayoutParams.width == mMinWidth && up) {
-            ViewGroup.LayoutParams paddingViewLayoutParams = mPaddingView.getLayoutParams();
-            paddingViewLayoutParams.height++;
-
-            if (headRect.top < 0) {
-                paddingViewLayoutParams.height = mStatusBarHeight;
-            }
-
-            if (paddingViewLayoutParams.height >= mStatusBarHeight) {
-                paddingViewLayoutParams.height = mStatusBarHeight;
-            }
-            mPaddingView.setLayoutParams(paddingViewLayoutParams);
-        }
-
-        if (!up) {
-            ViewGroup.LayoutParams paddingViewLayoutParams = mPaddingView.getLayoutParams();
-            paddingViewLayoutParams.height--;
-            if (rect.top == mFixedTop) {
-                paddingViewLayoutParams.height = 0;
-            }
-            if (paddingViewLayoutParams.height <= 0) {
-                paddingViewLayoutParams.height = 0;
-            }
-            mPaddingView.setLayoutParams(paddingViewLayoutParams);
-        }
-    }
 
     @Override
     public boolean onLayoutChild(CoordinatorLayout parent, AppBarLayout abl, int layoutDirection) {
@@ -211,6 +170,11 @@ public class DailyCoordinatorBehavior extends AppBarLayout.Behavior {
         mHeaderView = ((View) parent.getParent()).findViewById(R.id.header_view);
         mPaddingView = ((View) parent.getParent()).findViewById(R.id.padding);
         mScrolledView = ((View) parent.getParent()).findViewById(R.id.scrolled_view);
+
+        if (mHeaderHeight == 0) {
+            mHeaderHeight = mHeaderView.getMeasuredHeight();
+        }
+
         if (mScrolledWidth == 0) {
             mScrolledWidth = mScrolledView.getMeasuredWidth();
             ((View) mScrolledView.getParent()).bringToFront();
