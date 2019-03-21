1a13df18be974026ca753d51f781cc937f72926c
李欣科 <lixink@8531.cn>
2019-03-21 16:50:47 +0800

    添加渐变

---
+++ app/src/main/java/com/daily/android/TestApplication.java
@@ -0,0 +1,10 @@
+package com.daily.android;
+
+import android.support.multidex.MultiDexApplication;
+
+public class TestApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
